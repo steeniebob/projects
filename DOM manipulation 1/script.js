40fbd70d79404cd259a0657ae7b02230a8aaeb21
steeniebob <123820516+steeniebob@users.noreply.github.com>
2023-01-28 09:18:49 -0600

    Update script.js

--- DOM manipulation 1/script.js
+++ DOM manipulation 1/script.js
@@ -54,3 +54,16 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".number").textContent = guess;
   }
 });
+
+document.querySelector(".again").addEventListener("click", function () {
+  score = 20;
+  randomNum = Math.floor(Math.random() * 20 + 1);
+
+  document.querySelector(".message").textContent = "Start guessing...";
+  document.querySelector(".score").textContent = score;
+  document.querySelector(".number").textContent = "?";
+  document.querySelector(".guess").value = "";
+
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
+});
