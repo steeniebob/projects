88e37eff85c30d0d3f96eeb8bc82dfc570b4ff5a
steeniebob <123820516+steeniebob@users.noreply.github.com>
2023-01-29 19:27:01 -0600

    Update script.js

--- DOM manipulation 1/script.js
+++ DOM manipulation 1/script.js
@@ -18,24 +18,33 @@ console.log(document.querySelector(".guess").value);
 
 //event listener is something that happens on the page - a mouse click, keypress, etc. We can wait for a certain event to happen and then run the code. 
 */
+
 let randomNum = Math.floor(Math.random() * 20 + 1);
 let score = 20;
+let highscore = 0;
+
+const displayMessage = function (message) {
+  document.querySelector(".message").textContent = message;
+};
 
 document.querySelector(".check").addEventListener("click", function () {
   const guess = Number(document.querySelector(".guess").value);
   console.log(guess);
 
+  //check if guess is too high
   if (!guess) {
-    document.querySelector(".message").textContent = "😖 No number!";
-  } else if (guess > randomNum) {
+    displayMessage("😖 No number!");
+  } else if (guess != randomNum) {
     if (score > 1) {
-      document.querySelector(".message").textContent = "👇🏼 Too high!";
+      displayMessage(guess > randomNum ? "👇🏼 Too high!" : "👇🏼 Too low!");
       score--;
       document.querySelector(".score").textContent = score;
     } else {
-      document.querySelector(".message").textContent = "You lost the game!";
+      displayMessage("You lost the game!");
       document.querySelector(".score").textContent = 0;
     }
+
+    /* //check if guess is too low
   } else if (guess < randomNum) {
     if (score > 1) {
       document.querySelector(".message").textContent = "👇🏼 Too low!";
@@ -44,15 +53,15 @@ document.querySelector(".check").addEventListener("click", function () {
     } else {
       document.querySelector(".message").textContent = "You lost the game!";
       document.querySelector(".score").textContent = 0;
-    }
+    } */
   } else if (guess === randomNum) {
-    document.querySelector(".message").textContent = "👐🏼 Correct number!";
+    displayMessage("👐🏼 Correct number!");
     document.querySelector(".highscore").textContent = score;
     //when player wins
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
     document.querySelector(".number").textContent = guess;
-    
+
     if (score > highscore) {
       highscore = score;
       document.querySelector(".highscore").textContent = highscore;
@@ -64,7 +73,7 @@ document.querySelector(".again").addEventListener("click", function () {
   score = 20;
   randomNum = Math.floor(Math.random() * 20 + 1);
 
-  document.querySelector(".message").textContent = "Start guessing...";
+  displayMessage("Start guessing...");
   document.querySelector(".score").textContent = score;
   document.querySelector(".number").textContent = "?";
   document.querySelector(".guess").value = "";
